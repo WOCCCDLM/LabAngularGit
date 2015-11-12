9165070847669e98e741fe07d077e91908f2c40f
WOCCCDLM <wocccdlm@gmail.com>
2015-11-12 01:09:56 -0500

    proyecto actualizado

--- src/main/java/edu/eci/arsw/controller/EntryController.java
+++ src/main/java/edu/eci/arsw/controller/EntryController.java
@@ -23,13 +23,35 @@ public class EntryController {
 
     @RequestMapping(method = RequestMethod.GET,value = "/blogs")
     public List<Entry> getEntries() {
+	System.out.print("LLEGEEEEEEEEEEEE AL LISTADO"); 
         return entries;
     }
 
     @RequestMapping(method = RequestMethod.POST,value = "/blog")
     public  ResponseEntity<?>  postEntry(@RequestBody Entry p) {
+System.out.print("LLEGEEEEEEEEEEEE al agregar"); 
         entries.add(p);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
+     
+    @RequestMapping(method = RequestMethod.DELETE,value = "/blog")
+    public  void delete(@RequestBody int p) {
+	System.out.print("LLEGEEEEEEEEEEEE A ELIMINAR");        
+	entries.remove(p);
+        
+    }
 
+   @RequestMapping(method = RequestMethod.PUT,value = "/blog/{ind}")
+    public  List<Entry> editar(@RequestBody int p, @RequestBody Entry e) {
+	System.out.print("LLEGEEEEEEEEEEEE A Editar");        
+	/*String auxT, auxC;
+	Entry auxEntry;
+	auxT = e.getTitle();
+	auxC = e.getContent();*/
+	entries.set(p, e);
+	return entries;
+
+	
+        
+    }
 }
--- src/main/resources/static/js/app/controller/blogctrl.js
+++ src/main/resources/static/js/app/controller/blogctrl.js
@@ -1,4 +1,4 @@
-var app = angular.module("app",[]);
+﻿var app = angular.module("app",[]);
 
 app.controller("blogCtrl", function($scope,$log,$http) {
     $scope.entry = {title : "Title",
@@ -6,21 +6,58 @@ app.controller("blogCtrl", function($scope,$log,$http) {
     $scope.entries = [];
     $log.debug('se creo el $scope');
 
-    $scope.loadData = function() {
-        var configList = {
-                method: "GET",
-                url: "blogs"
-                    };
+   
 
-        var response=$http(configList);
 
-        response.success(function(data, status, headers, config) {
+ $scope.loadData = function() {
+        $http({
+            method: "GET",
+            url: "http://localhost:8080/blogs"
+          }).success(function(data) {
             $scope.entries = data;
-            });
-
-        response.error(function(data, status, headers, config) {
+        }).error(function(data,status,headers,config) {
             alert("Ha fallado la petición. Estado HTTP:"+status);
-            });
+        });
     };
-    $scope.loadData();
+
+
+ $scope.processForm = function() {
+        $log.debug($scope.entry);
+        $http({
+            method  : 'POST',
+            url     : 'http://localhost:8080/blog',
+            data    : $scope.entry,
+        }).success(function(data) {
+            console.log(data);
+            $scope.loadData();
+        });
+    };
+
+ $scope.eliminar = function(obj) {
+	alert(obj);
+        $log.debug($scope.entries[obj]);   
+        $http({
+            method  : 'DELETE',
+            url     : 'http://localhost:8080/blog',
+            data    : obj,
+	headers: {"Content-Type": "application/json;charset=utf-8"}
+        }).success(function() { 
+            $scope.loadData(); 
+		
+        });
+    };
+
+ $scope.editar = function(obj) {
+	alert(obj);
+        $log.debug($scope.entries[obj]);   
+        $http({
+            method  : 'PUT',
+            url     : 'http://localhost:8080/blogs',
+            data    : obj,
+        }).success(function() { 
+            $scope.loadData(); 
+		
+        });
+    };
+
 });
